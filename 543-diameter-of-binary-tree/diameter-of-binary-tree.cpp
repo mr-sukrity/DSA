f9f286d2eb7e537a00b8dc4e4ffecb6cfa6b5b76
Sukrity Roy <sukrityroy2022@gmail.com>
2026-07-25 22:10:26 +0530

    Time: 4 ms (20.01%) | Memory: 23.5 MB (94.43%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
@@ -6,31 +6,28 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    int height(TreeNode* root) {
-        if (root == NULL) {
-            return 0;
-        }
+        int ans = 0;
 
-        int leftHt = height(root->left);
-        int rightHt = height(root->right);
-
-        return max(leftHt, rightHt) + 1;
+int height(TreeNode* root) { //O(n)
+    if(root == NULL) {
+        return 0;
     }
 
-    int diameterOfBinaryTree(TreeNode* root) {
-        if (root == NULL) {
-            return 0;
-        }
+    int leftHt = height(root->left);
+    int rightHt = height(root->right);
 
-        int leftDiam = diameterOfBinaryTree(root->left);
-        int rightDiam = diameterOfBinaryTree(root->right);
-        int currDiam = height(root->left) + height(root->right);
-        return max(max(leftDiam, rightDiam), currDiam);
+    ans = max(ans, leftHt + rightHt); //currDiam of root node
+    return max(leftHt, rightHt) + 1;
+}
+
+int diameterOfBinaryTree(TreeNode* root) {
+    height(root);
+    return ans;
+
     }
 };
